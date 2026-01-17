c7208f21a86067f108138090a9ad20853d2081f9
Karl Sander <kall@kall.ws>
2026-01-17 23:48:13 +0100

    Initial Commit

---
+++ SuperSimple/SuperSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSimpleApp.swift
+//  SuperSimple
+//
+//  Created by Andreas Sander on 17.01.26.
+//
+
+import SwiftUI
+
+@main
+struct SuperSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
